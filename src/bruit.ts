c9458f776eae5492cfff0bf22157f1fd50ac64ee
Louis AMSTUTZ <louis.moventes@gmail.com>
2019-07-17 18:12:58 +0200

    first commit

---
+++ src/bruit.ts
@@ -0,0 +1,5 @@
+export class Bruit {
+    static init(config: string) {
+        console.log(config);
+    }
+}
---
+++ src/start.ts
@@ -0,0 +1,4 @@
+
+import { Bruit } from './bruit';
+console.log('start is called');
+Bruit.init('yolo');
